8f785fbfcba5e4e86c6f02b777fd8d126629ce21
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2025-11-02 11:57:29 +0100

    added download and reset functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const noteEl = document.getElementById("note");
 const statusEl = document.getElementById("status");
+const downloadBtn = document.getElementById("downloadBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 let currentContent = ""; 
 
@@ -21,9 +23,40 @@ noteEl.addEventListener("blur", () => {
 
     setTimeout(() => {
         statusEl.textContent = "";
-    }, 2000);
+    }, 5000);
+
+    localStorage.setItem("noteContent", newContent);
+
 });
 
 window.addEventListener("DOMContentLoaded", () => {
     currentContent = noteEl.textContent;
+
+    const savedContent = localStorage.getItem("noteContent");
+
+    if(savedContent) {
+        noteEl.innerHTML = savedContent;
+        currentContent = savedContent;
+    }
 })
+
+downloadBtn.addEventListener("click", () => {
+    const content = noteEl.textContent;
+    const blob = new Blob([content], {type: 'text/plain'});
+    const url = URL.createObjectURL(blob);
+    const a  = document.createElement('a');
+    a.href = url;
+    a.download = 'my-note.txt';
+    a.click();
+    URL.revokeObjectURL(url);
+
+    statusEl.textContent = "Note downloaded successfully!";
+});
+
+resetBtn.addEventListener("click", () => {
+    noteEl.textContent = "";
+    localStorage.removeItem('noteContent');
+    currentContent = "";
+    statusEl.textContent = "Note reset successfully!"
+})
+
